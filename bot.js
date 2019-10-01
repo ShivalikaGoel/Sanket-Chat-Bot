fb0fcfcc405ec76ec82826a1340eff5706ee42b6
ShivalikaGoel <42302322+ShivalikaGoel@users.noreply.github.com>
2019-10-01 13:48:11 +0530

    d

--- bot.js
+++ bot.js
@@ -179,15 +179,17 @@ class AttachmentsBot extends ActivityHandler {
 //         const base64Image = Buffer.from(imageData).toString('base64');
            if(str ==='a')
            {
-   
-        const imageData = fs.readFileSync(path.join(__dirname, '/resources/finalex.png'));
-        const base64Image = Buffer.from(imageData).toString('base64');
+               reply.text= 'hey!It';
            }
             else
             {
-                  const imageData = fs.readFileSync(path.join(__dirname, '/resources/a.png'));
-        const base64Image = Buffer.from(imageData).toString('base64');
+                relpy.text='Whatsoever';
             }
+    
+        const imageData = fs.readFileSync(path.join(__dirname, '/resources/finalex.png'));
+        const base64Image = Buffer.from(imageData).toString('base64');
+           
+        
     
         return {
             name: 'a.png',
