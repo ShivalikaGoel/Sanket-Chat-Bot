d7381d3cc577d532ce59f7bbc434cd1ccb41a0d6
ShivalikaGoel <42302322+ShivalikaGoel@users.noreply.github.com>
2019-10-01 12:27:29 +0530

    final ly

--- index.js
+++ index.js
@@ -1,56 +1,100 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-const dotenv = require('dotenv');
 const path = require('path');
 const restify = require('restify');
-
-// Import required bot services.
-// See https://aka.ms/bot-services to learn more about the different parts of a bot.
 const { BotFrameworkAdapter } = require('botbuilder');
-// const { AttachmentsBot } = require('./bots/attachmentsBot');
+const { AttachmentsBot } = require('./bot');
 
-// This bot's main dialog.
-const { EchoBot } = require('./bot');
-
-// Import required bot configuration.
+// Read botFilePath and botFileSecret from .env file.
 const ENV_FILE = path.join(__dirname, '.env');
-dotenv.config({ path: ENV_FILE });
+require('dotenv').config({ path: ENV_FILE });
 
-// Create HTTP server
-const server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, () => {
-    console.log(`\n${ server.name } listening to ${ server.url }`);
-    console.log(`\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator`);
-    console.log(`\nTo talk to your bot, open the emulator select "Open Bot"`);
-});
-
-// Create adapter.
-// See https://aka.ms/about-bot-adapter to learn more about how bots work.
+// Create adapter. See https://aka.ms/about-bot-adapter to learn more about adapters.
 const adapter = new BotFrameworkAdapter({
-    appId: process.env.MicrosoftAppId,
-    appPassword: process.env.MicrosoftAppPassword,
-    channelService: process.env.ChannelService,
-    openIdMetadata: process.env.BotOpenIdMetadata
+    appId: process.env.MicrosoftAppID,
+    appPassword: process.env.MicrosoftAppPassword
 });
 
 // Catch-all for errors.
 adapter.onTurnError = async (context, error) => {
-    // This check writes out errors to console log .vs. app insights.
+    // Write out errors to console log.
+    // NOTE: In production environment, you should consider logging this to Azure
+    //       application insights.
     console.error(`\n [onTurnError]: ${ error }`);
-    // Send a message to the user
+    // Send a message to the user.
     await context.sendActivity(`Oops. Something went wrong!`);
 };
 
-// Create the main dialog.
-const bot = new EchoBot();
-// const bot = new AttachmentsBot();
+// Create the AttachmentsBot.
+const bot = new AttachmentsBot();
 
+// Create HTTP server
+const server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function() {
+    console.log(`\n${ server.name } listening to ${ server.url }`);
+    console.log(`\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator`);
+});
 
 // Listen for incoming requests.
-server.post('/api/messages', (req, res) => {
-    adapter.processActivity(req, res, async (context) => {
-        // Route to main dialog.
+server.post('/api/messages', async (req, res) => {
+    await adapter.processActivity(req, res, async (context) => {
         await bot.run(context);
     });
-});
+})
+// // Copyright (c) Microsoft Corporation. All rights reserved.
+// // Licensed under the MIT License.
+//
+// const dotenv = require('dotenv');
+// const path = require('path');
+// const restify = require('restify');
+//
+// // Import required bot services.
+// // See https://aka.ms/bot-services to learn more about the different parts of a bot.
+// const { BotFrameworkAdapter } = require('botbuilder');
+// // const { AttachmentsBot } = require('./bots/attachmentsBot');
+//
+// // This bot's main dialog.
+// const { EchoBot } = require('./bot');
+//
+// // Import required bot configuration.
+// const ENV_FILE = path.join(__dirname, '.env');
+// dotenv.config({ path: ENV_FILE });
+//
+// // Create HTTP server
+// const server = restify.createServer();
+// server.listen(process.env.port || process.env.PORT || 3978, () => {
+//     console.log(`\n${ server.name } listening to ${ server.url }`);
+//     console.log(`\nGet Bot Framework Emulator: https://aka.ms/botframework-emulator`);
+//     console.log(`\nTo talk to your bot, open the emulator select "Open Bot"`);
+// });
+//
+// // Create adapter.
+// // See https://aka.ms/about-bot-adapter to learn more about how bots work.
+// const adapter = new BotFrameworkAdapter({
+//     appId: process.env.MicrosoftAppId,
+//     appPassword: process.env.MicrosoftAppPassword,
+//     channelService: process.env.ChannelService,
+//     openIdMetadata: process.env.BotOpenIdMetadata
+// });
+//
+// // Catch-all for errors.
+// adapter.onTurnError = async (context, error) => {
+//     // This check writes out errors to console log .vs. app insights.
+//     console.error(`\n [onTurnError]: ${ error }`);
+//     // Send a message to the user
+//     await context.sendActivity(`Oops. Something went wrong!`);
+// };
+//
+// // Create the main dialog.
+// const bot = new EchoBot();
+// // const bot = new AttachmentsBot();
+//
+//
+// // Listen for incoming requests.
+// server.post('/api/messages', (req, res) => {
+//     adapter.processActivity(req, res, async (context) => {
+//         // Route to main dialog.
+//         await bot.run(context);
+//     });
+// });
