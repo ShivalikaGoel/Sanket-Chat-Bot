ed55b7e9ae8a90867d3691134c706481687d5d2c
Aayushee25 <46399813+Aayushee25@users.noreply.github.com>
2019-10-04 17:19:34 +0530

    update

--- bot.js
+++ bot.js
@@ -14,14 +14,9 @@ class AttachmentsBot extends ActivityHandler {
             const membersAdded = context.activity.membersAdded;
             for (let cnt = 0; cnt < membersAdded.length; cnt++) {
                 if (membersAdded[cnt].id !== context.activity.recipient.id) {
-                    // If the Activity is a ConversationUpdate, send a greeting message to the user.
+                  
                    await context.sendActivity('Hey! My name is SANKET and I am here to help you out in your conversation. Just type the message you want to convey and I will convert it into sign language for you.');
-                  // await context.sendActivity('Alternatively, I can send you an attachment.');
-
-                    // Send a HeroCard with potential options for the user to select.
-                   // await this.displayOptions(context);
-
-                    // By calling next() you ensure that the next BotHandler is run.
+              
                     await next();
                 }
             }
@@ -33,22 +28,16 @@ class AttachmentsBot extends ActivityHandler {
                 // The user sent an attachment and the bot should handle the incoming attachment.
                 await this.handleIncomingAttachment(context);
             } else {
-                // Since no attachment was received, send an attachment to the user.
+              
                 await this.handleOutgoingAttachment(context);
             }
-            // Send a HeroCard with potential options for the user to select.
-            //await this.displayOptions(context);
-
-            // By calling next() you ensure that the next BotHandler is run.
+           
+          
             await next();
         });
     }
 
-    /**
-     * Saves incoming attachments to disk by calling `this.downloadAttachmentAndWrite()` and
-     * responds to the user with information about the saved attachment or an error.
-     * @param {Object} turnContext
-     */
+   
     async handleIncomingAttachment(turnContext) {
         // Prepare Promises to download each attachment and then execute each Promise.
         const promises = turnContext.activity.attachments.map(this.downloadAttachmentAndWrite);
@@ -67,21 +56,17 @@ class AttachmentsBot extends ActivityHandler {
             }
         }
 
-        // Prepare Promises to reply to the user with information about saved attachments.
-        // The current TurnContext is bound so `replyForReceivedAttachments` can also send replies.
+       
         const replyPromises = successfulSaves.map(replyForReceivedAttachments.bind(turnContext));
         await Promise.all(replyPromises);
     }
 
-    /**
-     * Downloads attachment to the disk.
-     * @param {Object} attachment
-     */
+   
     async downloadAttachmentAndWrite(attachment) {
-        // Retrieve the attachment via the attachment's contentUrl.
+      
         const url = attachment.contentUrl;
 
-        // Local file path for the bot to save the attachment.
+      
         const localFileName = path.join(__dirname, attachment.name);
 
         try {
@@ -102,61 +87,37 @@ class AttachmentsBot extends ActivityHandler {
             console.error(error);
             return undefined;
         }
-        // If no error was thrown while writing to disk, return the attachment's name
-        // and localFilePath for the response back to the user.
+       
         return {
             fileName: attachment.name,
             localPath: localFileName
         };
     }
 
-    /**
-     * Responds to user with either an attachment or a default message indicating
-     * an unexpected input was received.
-     * @param {Object} turnContext
-     */
+  
     async handleOutgoingAttachment(turnContext) {
         const reply = { type: ActivityTypes.Message };
 
-        // Look at the user input, and figure out what type of attachment to send.
-        // If the input matches one of the available choices, populate reply with
-        // the available attachments.
-        // If the choice does not match with a valid choice, inform the user of
-        // possible options.
+        
         const firstChar = turnContext.activity.text;
         
        
-//         if (firstChar === '1') {
+
             //reply.text =firstchar;
             reply.attachments = [this.getInlineAttachment(firstChar[0])];
        
-//         } else if (firstChar === '2') {
-//             reply.attachments = [this.getInternetAttachment()];
-//             reply.text = 'This is an internet attachment.';
-//         } else if (firstChar === '3') {
-//             reply.attachments = [await this.getUploadedAttachment(turnContext)];
-//             reply.text = 'This is an uploaded attachment.';
-//         } else {
-//             // The user did not enter input that this bot was built to handle.
-//             reply.text = 'Your input was not recognized, please try again.';
-//         }
+
         await turnContext.sendActivity(reply);
     }
 
-    /**
-     * Sends a HeroCard with choices of attachments.
-     * @param {Object} turnContext
-     */
+   
     async displayOptions(turnContext) {
         const reply = { type: ActivityTypes.Message };
         
-        // Note that some channels require different values to be used in order to get buttons to display text.
-        // In this code the emulator is accounted for with the 'title' parameter, but in other channels you may
-        // need to provide a value for other parameters like 'text' or 'displayText'.
+        
         const buttons = [
             { type: ActionTypes.ImBack, title: '1. Inline Attachment', value: '1' },
-//             { type: ActionTypes.ImBack, title: '2. Internet Attachment', value: '2' },
-//             { type: ActionTypes.ImBack, title: '3. Uploaded Attachment', value: '3' }
+
         ];
 
         const card = CardFactory.heroCard('', undefined,
